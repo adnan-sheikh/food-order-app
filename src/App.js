6235b394bbb21acd857432a24df765073ef1b316
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-06-07 17:10:26 +0530

    add Meals related components and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from './components/layout/Header';
 
+import Meals from './components/meals/Meals';
+
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 };
---
+++ src/components/meals/Meals.js
@@ -0,0 +1,15 @@
+import classes from './Meals.module.css';
+
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <div>
+      <MealsSummary />
+      <AvailableMeals />
+    </div>
+  );
+};
+
+export default Meals;
