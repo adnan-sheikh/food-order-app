58aa9551b3878192ce32c6ef35a04d5b2020a186
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-06-07 20:48:05 +0530

    add Cart component as Modal and related styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './components/layout/Header';
-
 import Meals from './components/meals/Meals';
+import Cart from './components/cart/Cart';
 
 const App = () => {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,11 +1,13 @@
 import classes from './Cart.module.css';
 
+import Modal from '../ui/Modal';
+
 const Cart = (props) => {
   const cartItems = [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(
     (item) => <li>{item.name}</li>
   );
   return (
-    <div>
+    <Modal>
       <ul className={classes['cart-items']}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -15,7 +17,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
