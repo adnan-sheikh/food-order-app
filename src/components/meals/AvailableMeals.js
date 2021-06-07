fa0acb124fdb30e8b84398d3ad4b37c7a8e3af37
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-06-07 19:10:12 +0530

    add MealItemForm component to MealItem

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import classes from './AvailableMeals.module.css';
 
+import Card from '../ui/Card';
+import MealItem from './MealItem/MealItem';
+
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -28,11 +31,21 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
