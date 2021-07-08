5026c74248564dca48be0b488dc8f89675198a9b
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-07-08 18:09:11 +0530

    handle error for fetching data

--- src/components/meals/AvailableMeals.js
+++ src/components/meals/AvailableMeals.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -14,6 +15,11 @@ const AvailableMeals = () => {
       const response = await fetch(
         'https://react-http-bd80c-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
       );
+
+      if (!response.ok) {
+        throw Error('Something went wrong...');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -29,13 +35,25 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
